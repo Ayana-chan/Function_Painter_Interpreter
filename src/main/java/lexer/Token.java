b63e6b855ac9e262891fdcc8036878e82ae9b635
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2022-12-11 00:14:51 +0800

    Token

---
+++ src/main/java/lexer/Token.java
@@ -0,0 +1,8 @@
+package lexer;
+
+public class Token {
+    TokenTypeEnum type;
+    String lexeme;//原始串内容
+    double value;//数值
+    String funName;//函数名
+}
--- src/main/java/lexer/TokenTypeEnum.java
+++ src/main/java/lexer/TokenTypeEnum.java
@@ -1,15 +1,17 @@
 package lexer;
 
-public enum TokenType {
+public enum TokenTypeEnum {
     ID,COMMENT,//参见正规式设计
     ORIGIN,SCALE,ROT,IS,//保留字
     TO,STEP,DRAW,FOR,FROM,//保留字
     T, //参数
     SEMICO,L_BRACKET,R_BRACKET,COMMA,//分隔符
     PLUS,MINUS,MUL,DIV,POWER,//运算符
-    FUNC,//函数名（调用)
-    CONST_ID, //常数（数值字面量、命名常量)
-    NONTOKEN,//源程序结束(#)
-    ERRTOKEN,//错误单词（非法输入
+    FUNC,//函数名（调用）
+    CONST_ID, //常数（数值字面量、命名常量）
+    NONTOKEN,//源程序结束（#）
+    ERRTOKEN,//错误单词（非法输入）
+
+    
 }
 
