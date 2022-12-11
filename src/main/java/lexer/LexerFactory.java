4d887557a1680f74f3b1102b6ea7063259a6bd31
DeathWind-sama <yychen_114@stu.xidian.edu.cn>
2022-12-11 13:08:36 +0800

    Better LexerFactory

--- src/main/java/lexer/LexerFactory.java
+++ src/main/java/lexer/LexerFactory.java
@@ -4,31 +4,34 @@ import java.io.*;
 
 public class LexerFactory {
     /**
-     *
+     * produce by fileName
      * @param fileName
      * @return return null if failed
      * @throws FileNotFoundException
      */
     public static Lexer getLexer(String fileName) throws FileNotFoundException {
         Lexer lexer=null;
+        //获得文件流
         FileInputStream fileInputStream=new FileInputStream(fileName);//throws FileNotFoundException
-        InputStreamReader inputStreamReader=null;
+        return getLexer(fileInputStream);
+    }
+
+    /**
+     * produce by fileInputStream
+     * @param fileInputStream
+     * @return return null if failed
+     */
+    public static Lexer getLexer(FileInputStream fileInputStream) {
+        Lexer lexer=null;
+        InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
         try {
-            inputStreamReader = new InputStreamReader(fileInputStream);
             //生产所需的Lexer。TxtReader会立刻把第一个字符读进currChar。
             TxtReader txtReader=new TxtReader(inputStreamReader);
             return new Lexer(txtReader);
         }catch (IOException e){
             e.printStackTrace();
-        }finally {
-            if(inputStreamReader!=null){
-                try {
-                    inputStreamReader.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
         }
+        //失败
         return null;
     }
 }
